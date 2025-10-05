f7712a4a6275f40cbabf1fce2ee4fa2b01c88cbd
Bradleyyx2 <bradleyaditya2705@gmail.com>
2025-10-05 22:24:47 +0700

    Edit Student.kt sesuai UML dan perbaikan Person

--- src/Student.kt
+++ src/Student.kt
@@ -1,17 +1,25 @@
-
 class Student(
     name: String,
-    age: Int
-) : Person(name, age) {
+    address: String,
+    phoneNumber: String,
+    var studentId: String
+) : Person(name, address, phoneNumber) {
 
     var courses: MutableList<Course> = mutableListOf()
     var organisation: StudentOrganisation? = null
 
     fun enrollCourse(course: Course) {
         courses.add(course)
+        course.addStudent(this) // biar hubungan 2 arah sesuai UML
     }
 
     fun joinOrganisation(organisation: StudentOrganisation) {
         this.organisation = organisation
+        organisation.addMember(this) // biar konsisten dengan UML
+    }
+
+    override fun display() {
+        super.display()
+        println("studentId = $studentId")
     }
 }

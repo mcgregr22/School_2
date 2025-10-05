06ef227b7426688441218330323334148441169f
Marenn13 <marennelloh@gmail.com>
2025-10-05 11:30:34 +0700

    Recreate Room class

--- src/Room.kt
+++ src/Room.kt
@@ -1,4 +1,60 @@
 class Room {
+    // Kelas Table
+    class Table(
+        val id: Int,
+        val material: String
+    ) {
+        fun info() {
+            println("Table ID: $id, Material: $material")
+        }
+    }
 
+    // Kelas Chair
+    class Chair(
+        val id: Int,
+        val hasCushion: Boolean
+    ) {
+        fun info() {
+            println("Chair ID: $id, Has Cushion: $hasCushion")
+        }
+    }
+
+    // Kelas Staff
+    class Staff(
+        val name: String,
+        val role: String
+    ) {
+        fun info() {
+            println("Staff Name: $name, Role: $role")
+        }
+    }
+
+    // Kelas Room
+    class Room(
+        val tableList: MutableList<Table> = mutableListOf(),
+        val chairList: MutableList<Chair> = mutableListOf(),
+        var CSStaff: Staff
+    ) {
+        fun addTable(table: Table) {
+            tableList.add(table)
+            println("Added table with ID: ${table.id}")
+        }
+
+        fun addChair(chair: Chair) {
+            chairList.add(chair)
+            println("Added chair with ID: ${chair.id}")
+        }
+
+        fun displayRoomInfo() {
+            println("Room Information:")
+            CSStaff.info()
+
+            println("\nTables:")
+            tableList.forEach { it.info() }
+
+            println("\nChairs:")
+            chairList.forEach { it.info() }
+        }
+    }
 
 }
